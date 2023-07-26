b54ed54c2891695ab2fc314b17508704362053f1
lbluefishl <84884872+lbluefishl@users.noreply.github.com>
2023-07-26 20:46:35 +1000

    modify server port

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
 
 app.use(express.json()); // Add this line to parse JSON request bodies
 
