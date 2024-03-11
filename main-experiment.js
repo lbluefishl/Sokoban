d7b9c582ca7c18ae7bbc03e11160d47d879bc458
lbluefishl <lbluefishl@yahoo.com>
2024-03-12 10:22:04 +1100

    Revert "fixes, control group no survey"
    This reverts commit a938746d0c93e3e27b98dc54324d733ced914d47.

--- main-experiment.js
+++ main-experiment.js
@@ -330,6 +330,7 @@ function checkWinCondition() {
   recordUserCompletion();
   determineNextLevel();
   clearLocalStorageExceptPlayerId();
+  storeLevelNumber();
   handleEarlyCompletion();
 }
 
@@ -339,13 +340,15 @@ function handleEarlyCompletion() {
   const practiceTrials = conditions.length > 3;
   const controlCondition = conditions[0] == 1;
   if (!practiceTrials && !controlCondition && !allTrialsDone)
-  {
     showPopup("Please respond to the following statements about your current status and feelings towards the puzzle you were working on. You will then take a short break before working on the next puzzle.", "break");
-    storeLevelNumber();
+
+  else if (!practiceTrials && controlCondition && !allTrialsDone)
+  {
+    removeCondition();
+    showPopup("Respond to the following statements with the option which best represents how you currently feel. You will then work on the next puzzle.", "control");
   }
   else 
   {
-    storeLevelNumber();
     removeCondition();
     generateNewLevel();
   }
@@ -740,6 +743,7 @@ function showPopup(message, type) {
     removePopup();
     recordTimeAfterBreak();
     resetTimer();
+    if (levelComplete == 1) generateNewLevel();
   }
 
   function handleBreakClick(event) {
